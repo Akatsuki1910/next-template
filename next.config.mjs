d7e4c02ffd5166bdb791fea0a311df166426896a
Akatsuki1910 <35941346+Akatsuki1910@users.noreply.github.com>
2024-06-27 16:28:27 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
+const isProd = process.env.NODE_ENV === 'production';
+const prefixPath = isProd ? '/next-template' : '';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  assetPrefix: prefixPath,
+  basePath: prefixPath,
   distDir: 'dist',
 };
 
